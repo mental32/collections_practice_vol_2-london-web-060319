14579008a294ba415d05c437aa174cbaccb2a0c0
beatific-revision-5216 <ivanov_chris@yahoo.com>
2019-05-30 02:59:16 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -83,6 +83,8 @@ describe '#remove_non_strings' do
 
 end
 
+def count_elements
+
 describe '#count_elements' do
 
   # Question 5
@@ -92,58 +94,6 @@ describe '#count_elements' do
   end
 
 end
-
-let(:keys) {
-  [
-      {
-          :first_name => "blake",
-               :motto => "Have a koala-ty day!"
-      },
-      {
-          :first_name => "ashley",
-               :motto => "I dub thee, 'Lady Brett Ashley'."
-      }
-  ]
-}
-
-let(:data) {
-  [
-         {
-           "blake" => {
-              :awesomeness => 10,
-                   :height => "74",
-                :last_name => "johnson"
-          },
-          "ashley" => {
-              :awesomeness => 9,
-                   :height => 60,
-                :last_name => "dubs"
-          }
-      }
-  ]
-}
-
-let(:merged_data) {
-  [
-      {
-           :first_name => "blake",
-          :awesomeness => 10,
-               :height => "74",
-            :last_name => "johnson",
-                :motto => "Have a koala-ty day!"
-
-      },
-      {
-           :first_name => "ashley",
-          :awesomeness => 9,
-               :height => 60,
-            :last_name => "dubs",
-                :motto => "I dub thee, 'Lady Brett Ashley'."
-
-      }
-  ]
-}
-
 
 def merge_data(keys, data)
   collection = data.values.reduce({}, :merge)
