1e5df98dfe7ae0859c811f78089292591db37970
beatific-revision-5216 <ivanov_chris@yahoo.com>
2019-05-30 03:19:17 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -23,7 +23,7 @@ def count_elements(arr)
 end
 
 def merge_data(keys, data)
-  collection = data.values.reduce({}, :merge)
+  collection = data.reduce({}, :merge)
   Hash[ *collection.keys.map { |k| collection[k].merge Hash[:motto, keys.find { |d| d[:first_name] == k }] } ]
 end
 
