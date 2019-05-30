092ab8bab4c4a42b1dba4eb4e8f67839eb108c24
beatific-revision-5216 <ivanov_chris@yahoo.com>
2019-05-30 03:27:09 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,30 +1,30 @@
-let(:schools) {
-  {
-    "flatiron school bk" => {
-      :location => "NYC"
-    },
-    "flatiron school" => {
-      :location => "NYC"
-    },
-    "dev boot camp" => {
-      :location => "SF"
-    },
-    "dev boot camp chicago" => {
-      :location => "Chicago"
-    },
-    "general assembly" => {
-      :location => "NYC"
-    },
-    "Hack Reactor" => {
-      :location => "SF"
-    }
-  }
-}
-let(:organized_schools) {
-  {"NYC"=>["flatiron school bk", "flatiron school", "general assembly"],
-   "SF"=>["dev boot camp", "Hack Reactor"],
-   "Chicago"=>["dev boot camp chicago"]}
-}
+# let(:schools) {
+#   {
+#     "flatiron school bk" => {
+#       :location => "NYC"
+#     },
+#     "flatiron school" => {
+#       :location => "NYC"
+#     },
+#     "dev boot camp" => {
+#       :location => "SF"
+#     },
+#     "dev boot camp chicago" => {
+#       :location => "Chicago"
+#     },
+#     "general assembly" => {
+#       :location => "NYC"
+#     },
+#     "Hack Reactor" => {
+#       :location => "SF"
+#     }
+#   }
+# }
+# let(:organized_schools) {
+#   {"NYC"=>["flatiron school bk", "flatiron school", "general assembly"],
+#   "SF"=>["dev boot camp", "Hack Reactor"],
+#   "Chicago"=>["dev boot camp chicago"]}
+# }
 
 def organize_schools(schools)
   schools.values
