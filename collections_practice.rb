7d6da590bfcb803d6c8e59cb8e7f0c29c32e1168
beatific-revision-5216 <ivanov_chris@yahoo.com>
2019-05-30 03:26:11 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,7 +1,5 @@
 def organize_schools(schools)
-  collection = Hash[*data]
-  collection
-  Hash[ *collection.values.uniq.map { |k| [k, [schools.select { |n, v| v[:location] == k } ] ] }.flatten(1) ]
+
 end
 
 def begins_with_r(arr)
